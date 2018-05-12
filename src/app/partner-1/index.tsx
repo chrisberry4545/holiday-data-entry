b789b722dffc9f00ee25d7d267d732cc902df7fd
Christopher <chrisberry4545@googlemail.com>
2018-05-12 16:19:15 +0100

    fix: removed service workers to stop index.html caching

--- src/app/partner-1/index.tsx
+++ src/app/partner-1/index.tsx
@@ -3,7 +3,4 @@ import * as ReactDOM from 'react-dom';
 
 import { Partner1Component } from './partner-1.component';
 
-import registerServiceWorker from './../register-service-workers';
-
 ReactDOM.render(<Partner1Component />, document.getElementById('root'));
-registerServiceWorker();
